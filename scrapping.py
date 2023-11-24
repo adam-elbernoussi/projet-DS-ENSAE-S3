b5c806ce0b169b34d538625330e824d716d04290
Adam El Bernoussi <adam.elbernoussi@ensae.fr>
2023-11-24 17:20:11 +0100

    completing scrapping.py

--- scrapping.py
+++ scrapping.py
@@ -10,15 +10,48 @@ import re
 import datetime
 import time
 
+ticker = "BNPP"
+name = "bnp-paribas"
+
 #Coding functions
+def scrap_article(link : str):
+    """
+    This function scraps an article from a given link.
 
+    Parameters
+    ----------
+    link : str
+        The link of the article.
 
-
-
-
-
-
-
+    Returns
+    -------
+    title : str
+        The title of the article.
+    date : str
+        The date of the article.
+    content : str
+        The content of the article.
+    """
+    #Getting the page
+    page = requests.get(link)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    #for title
+    res_title = soup.title.string
+    #for date
+    stringForDate = soup.find("span", {"class": "co_g2"}).string
+    stringForHour = re.findall(r"\d+/\d+/\d+ \d+:\d+", stringForDate)[0]
+    res_date = datetime.datetime.strptime(stringForHour, r"%d/%m/%y %H:%M")
+    #for content
+    article_body = soup.find("div", {"class": "txtbig content_news"})
+    article_body = article_body.p
+    listSentence = article_body.strings
+    res_content = ""
+    for sentence in listSentence:
+        res_content += sentence.string
+    res_content = re.sub(r"\.(?=\D)", ". ", res_content)
+    res_content = re.findall(r"- (.*) Copyright", res_content)[0]
+    print(res_date)
+    return {"title": res_title, "date": str(res_date), "content": res_content}
 
 
 
@@ -26,6 +59,6 @@ import time
 
 
 # main
-if __name__ == "__main__":
-    print("Hello world !")  
-    
+if __name__ == "__main__": 
+    print(scrap_article("https://www.abcbourse.com/marches/air-liquide-nouveaux-ppa-avec-sasol-en-afrique-du-sud_613216"))
+
