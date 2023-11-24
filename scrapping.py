fe3af840f20f071dae34e77ab83cdaf94c68195d
ADAM EL BERNOUSSI <adam.elbernoussi@ensae.Fr>
2023-11-24 13:44:53 +0100

    first commit initializing the repo

---
+++ scrapping.py
@@ -0,0 +1,31 @@
+"""In this file, we will scrap some articles about a company given its ticker."""
+"""Firstly, on the website Investing.com"""
+
+#Importing libraries
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import re
+import datetime
+import time
+
+#Coding functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# main
+if __name__ == "__main__":
+    print("Hello world !")  
+    
