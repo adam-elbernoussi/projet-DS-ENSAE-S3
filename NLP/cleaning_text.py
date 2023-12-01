fd6d3c01a687c42ee45ffa5954b92028b5244b91
ADAM EL BERNOUSSI <adam.elbernoussi@ensae.Fr>
2023-12-01 17:59:52 +0100

    Ok cleaning text part

--- NLP/cleaning_text.py
+++ NLP/cleaning_text.py
@@ -12,6 +12,7 @@ import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
 from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
 
 #Coding functions
 def tokenization(text : str):
@@ -20,4 +21,22 @@ def tokenization(text : str):
     words = [word for word in words if word.isalpha()]
     return words
 
+def remove_stop_words(words : list):
+    stop_words = set(stopwords.words('french'))
+    wordsNoStopWords = [word for word in words if word not in stop_words]
+    return wordsNoStopWords
+
+def stemming(words : list):
+    stemmer = SnowballStemmer("french")
+    wordsStemmed = [stemmer.stem(word) for word in words] 
+    return wordsStemmed
+
+if __name__ =="__main__":
+    text = """Air Liquide a annoncé mardi avoir signé de nouveaux contrats de long terme avec le sud-africain Sasol portant sur des capacités d'électricité renouvelable de 100 MW au niveau du site de Secunda. Le partenariat a été conclu avec le fournisseur d'énergie éolienne et solaire Mainstream Renewable Power, qui a été chargé de construire une ferme solaire qui devrait être opérationnelle en 2025. Il s'agit de la troisième série de 'PPA' conclus par Air Liquide et Sasol, après ceux déjà annoncés en début d'année avec Enel Green Power et avec TotalEnergies et son partenaire Mulilo. Ces accords doivent contribuer à l'objectif d'Air Liquide de réduire de 30% à 40% les émissions de CO2 liées à la production d'oxygène d'ici 2031 à Secunda, où le groupe exploite le plus grand site de production d'oxygène au monde. Pour mémoire, Air Liquide a racheté à Sasol les 16 unités de production d'oxygène de ce dernier à Secunda, qu'il exploite désormais depuis juin 2021. En tenant compte d'une autre unité de séparation des gaz de l'air (ASU) que le groupe opérait déjà pour Sasol, Air Liquide exploite un total de 17 ASU à Secunda, pour une capacité totale de 47.000 tonnes d'oxygène par jour."""
+    tokenList = tokenization(text)
+    tokenListNoStopWords = remove_stop_words(tokenList)
+    stemmedTokenList = stemming(tokenListNoStopWords)
+    print(tokenList)
+    print(tokenListNoStopWords)
+    print(stemmedTokenList)
 
--- tests/test_cleaning_text.py
+++ tests/test_cleaning_text.py
@@ -23,7 +23,7 @@ dumas = clean_text_(dumas)
 
 
 
-from cleaning_text import tokenization#, remove_stop_words, stemming
+from cleaning_text import tokenization, remove_stop_words, stemming
 
 class Test_tokenization(unittest.TestCase):
     def test_tokenization_type(self):
@@ -38,6 +38,38 @@ class Test_tokenization(unittest.TestCase):
     def test_tokenisation_other(self):
         res = tokenization("J'apprécie fortement le poulet sauce moutarde avec le riz rond et la sauce champignons.")
         self.assertEqual(res, ['fortement', 'le', 'poulet', 'sauce', 'moutarde', 'avec', 'le', 'riz', 'rond', 'et', 'la', 'sauce', 'champignons'])
+        self.assertIsInstance(res, list)
+        self.assertIsInstance(res[0], str)
+
+class Test_remove_stop_words(unittest.TestCase):
+    def test_remove_stop_words_type(self):
+        res = remove_stop_words(['Ce', 'matin', 'je', 'me', 'suis', 'baladé', 'dans', 'la', 'forêt', 'vu', 'un', 'lapin', 'et', 'un', 'écureuil', 'mangé', 'une', 'pomme'])
+        self.assertIsInstance(res, list)
+        self.assertIsInstance(res[0], str)
+
+    def test_remove_stop_words_content(self):
+        res = remove_stop_words(['Ce', 'matin', 'je', 'me', 'suis', 'baladé', 'dans', 'la', 'forêt', 'vu', 'un', 'lapin', 'et', 'un', 'écureuil', 'mangé', 'une', 'pomme'])
+        self.assertEqual(res, ['Ce', 'matin', 'baladé', 'forêt', 'vu', 'lapin', 'écureuil', 'mangé', 'pomme'])
+
+    def test_remove_stop_words_other(self):
+        res = remove_stop_words(['fortement', 'le', 'poulet', 'sauce', 'moutarde', 'avec', 'le', 'riz', 'rond', 'et', 'la', 'sauce', 'champignons'])
+        self.assertEqual(res, ['fortement', 'poulet', 'sauce', 'moutarde', 'riz', 'rond', 'sauce', 'champignons'])
+        self.assertIsInstance(res, list)
+        self.assertIsInstance(res[0], str)
+
+class Test_stemming(unittest.TestCase):
+    def test_stemming_type(self):
+        res = stemming(['Ce', 'matin', 'baladé', 'forêt', 'vu', 'lapin', 'écureuil', 'mangé', 'pomme'])
+        self.assertIsInstance(res, list)
+        self.assertIsInstance(res[0], str)
+
+    def test_stemming_content(self):
+        res = stemming(['Ce', 'matin', 'baladé', 'forêt', 'vu', 'lapin', 'écureuil', 'mangé', 'pomme'])
+        self.assertEqual(res, ['ce', 'matin', 'balad', 'forêt', 'vu', 'lapin', 'écureuil', 'mang', 'pomm'])
+
+    def test_stemming_other(self):
+        res = stemming(['fortement', 'poulet', 'sauce', 'moutarde', 'riz', 'rond', 'sauce', 'champignons'])
+        self.assertEqual(res, ['fort', 'poulet', 'sauc', 'moutard', 'riz', 'rond', 'sauc', 'champignon'])
         self.assertIsInstance(res, list)
         self.assertIsInstance(res[0], str)
 
