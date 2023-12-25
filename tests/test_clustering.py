a1d89bd45a556373d2f8c7d20d140ac924b5944d
Adam El Bernoussi <adam.elbernoussi@ensae.fr>
2023-12-25 03:19:24 +0100

    re

--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -6,6 +6,7 @@ from NLP.clustering import pipeline
 
 class Test_pipeline(unittest.TestCase):
     def test_pipeline(self):
+        #to do
         self.assertEqual(True, True)
 
 if __name__ == "__main__":
